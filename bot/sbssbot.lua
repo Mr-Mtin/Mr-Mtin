d558594efc9beab3263fbd98122eac554ec86e8d
Mr-Mtin <webamoz.com@gmail.com>
2016-03-28 16:23:20 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "webshot",
     "welcome"
     },
-    sudo_users = {122774063},--Sudo users
+    sudo_users = {177831277},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V3
